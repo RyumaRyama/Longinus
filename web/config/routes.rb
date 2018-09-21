a80c795b127f472206d086e3ac28e631f740369d
RyumaRyama <homes.ryu@gmail.com>
2018-09-21 19:57:14 +0900

    copy from docker branch

---
+++ web/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'user_profile/home'
+  get 'user_profile/help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
