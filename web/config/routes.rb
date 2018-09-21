adc5f16fa1d5bc9f7e884ca0a5390469ceaad5d9
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-09-21 19:20:31 +0900

    delete all files

--- web/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'user_profile/home'
-  get 'user_profile/help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
-end
