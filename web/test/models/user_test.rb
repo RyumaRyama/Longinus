caceb44a4d3daaf737494657412c4b428c536b8b
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-03 23:43:50 +0900

    テスト関数名の日本語化

--- web/test/models/user_test.rb
+++ web/test/models/user_test.rb
@@ -6,31 +6,31 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
-  test "should be valid" do
+  test "User認証されている" do
     assert @user.valid?
   end
 
-  test "name should be present" do
+  test "nameが存在する" do
     @user.name = "     "
     assert_not @user.valid?
   end
 
-  test "email should be present" do
+  test "emailが存在する" do
     @user.email = "     "
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
+  test "nameが長すぎない" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
 
-  test "email should not be too long" do
+  test "emailが長すぎない" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
+  test "emailの認証では，有効なアドレスのみを受け付ける" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -39,19 +39,19 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test "emailアドレスが一意である" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
 
-  test "password should be present (nonblank)" do
+  test "passwordが空白でない" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
-  test "password should have a minimum length" do
+  test "passwordが少なすぎない" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
