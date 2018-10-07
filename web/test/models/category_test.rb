752cd714025f270a6aeeade813cea2718b3e1d9a
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-08 00:11:40 +0900

    Category Modelの作成

---
+++ web/test/models/category_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  def setup
+    @category = Category.new(name: "food")
+  end
+
+  test "name should be present" do
+    @category.name = "     "
+    assert_not @category.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 11
+    assert_not @category.valid?
+  end
+end
