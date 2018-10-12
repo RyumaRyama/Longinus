790ee43392ba2bcbd0760bb34f5854895ec9944e
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-12 23:03:01 +0900

    passwordカラムの追加

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_130108) do
+ActiveRecord::Schema.define(version: 2018_10_12_140020) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_10_12_130108) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
