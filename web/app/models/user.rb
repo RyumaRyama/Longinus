56892689850e877cdc234f39b1cec2c5148e97a2
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-20 16:41:11 +0900

    model図の作成

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # email最大255文字，password最低6文字のユーザー情報
   # has_many :user_introductions, dependent: :destroy
   has_and_belongs_to_many :categories, :join_table => 'categories_users'
-  has_and_belongs_to_many :elements,  :join_table => 'categories_users'
   before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
