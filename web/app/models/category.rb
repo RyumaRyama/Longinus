9c31f8a5a8654bcd199ff423520f4779d39e40da
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-21 00:30:43 +0900

    UserIntroducitonModelとCategoryModelの削除

--- web/app/models/category.rb
+++
@@ -1,6 +0,0 @@
-class Category < ApplicationRecord
-  has_many :elements
-  # has_many :user_introductions
-  # has_and_belongs_to_many :users, :join_table => 'categories_users'
-  validates :name, presence: true, length: { maximum: 10 }, uniqueness: true
-end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_120121) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_10_20_152414) do
 
   create_table "elements", force: :cascade do |t|
     t.string "name"
@@ -29,17 +23,6 @@ ActiveRecord::Schema.define(version: 2018_10_19_120121) do
     t.integer "user_id", null: false
     t.index ["element_id"], name: "index_elements_users_on_element_id"
     t.index ["user_id"], name: "index_elements_users_on_user_id"
-  end
-
-  create_table "user_introductions", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "category_id"
-    t.integer "element_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_user_introductions_on_category_id"
-    t.index ["element_id"], name: "index_user_introductions_on_element_id"
-    t.index ["user_id"], name: "index_user_introductions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
