5a509916ec27e6baa48c7592bfc48769a807bd62
RyumaRyama <homes.ryu@gmail.com>
2018-10-23 20:20:25 +0900

    登録画面の分割

---
+++ web/test/controllers/element_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ElementControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get element_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get element_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get element_update_url
+    assert_response :success
+  end
+
+end
