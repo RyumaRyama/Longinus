c8c01c8c3e0ad03307cafbff4f8612a77d0a71fc
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-10-30 21:02:30 +0900

    created login

--- web/app/controllers/user_profile_controller.rb
+++ web/app/controllers/user_profile_controller.rb
@@ -19,6 +19,7 @@ class UserProfileController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       redirect_to new_element_path
     else
       render 'new'
