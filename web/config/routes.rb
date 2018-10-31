a42b536a0b6562d3cc708693aed605f31c4139b4
RyumaRyama <homes.ryu@gmail.com>
2018-10-31 12:49:41 +0900

    modified routes.rb

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get 'user_profile/gussy'
   get '/signup',  to: 'user_profile#new'
   post '/signup', to: 'user_profile#create',:as => 'user'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :user_profile
   resources :element
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
