72cfb0cf8599f5cdc0577d512a0a28197a028ef8
RyumaRyama <homes.ryu@gmail.com>
2018-11-05 20:54:25 +0900

    create common_elements

--- web/app/controllers/common_elements_controller.rb
+++ web/app/controllers/common_elements_controller.rb
@@ -1,4 +1,7 @@
 class CommonElementsController < ApplicationController
   def show
+    friend = User.find_by(id: params[:id])
+    @friend_name = friend.name
+    @friend_elements = friend.elements.where(id: current_user.elements)
   end
 end
