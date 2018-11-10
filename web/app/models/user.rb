fbf64d36598d5e3c5dd633d5456a721ffb41dce4
RyumaRyama <homes.ryu@gmail.com>
2018-11-10 23:55:00 +0900

    user can follow

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -17,5 +17,14 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
 end
 
