aab632d31d4bc6b949f46232cbb0a061d44864f9
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-11-17 18:04:07 +0900

    自身のページでprivateの項目は印をつけて表示

--- web/app/controllers/element_controller.rb
+++ web/app/controllers/element_controller.rb
@@ -12,29 +12,17 @@ class ElementController < ApplicationController
   end
 
   def edit
-    # @user = User.find(current_user.id)
     @user = User.find(params[:id])
     @element = Element.new
 
     if @user.elements.size == 0
       redirect_to new_element_path
     end
-    # 2.times { @user.elements.build }
-    # @elements = User.find(current_user.id).elements
-    # @elements = Element.find(user_elements.ids)
-    # p @elements
-    # puts '*' * 100
   end
 
   def update
     @user = User.find(params[:id])
-    # @user.elements << Element.find_by(update_user_elements_params)
-    # if @user.update_attributes(update_user_elements_params)
-    #   redirect_to root_path
-    # else
-    #   render 'edit'
-    # end
-    
+
     update_user_elements_params[:elements_attributes].each do |element|
       if element[1][:_destroy] == "1"
         @user.elements.delete(Element.find_by(id: element[1][:id]))
@@ -53,7 +41,10 @@ class ElementController < ApplicationController
     end
 
     def update_user_elements_params
-      params.require(:user).permit(users_elements: [:private,:id],elements_attributes: [:name, :id, :_destroy])
+      params.require(:user).permit(
+          users_elements: [:private,:id],
+          elements_attributes: [:name, :id, :_destroy]
+      )
     end
 
     def add_elements(message)
@@ -70,5 +61,4 @@ class ElementController < ApplicationController
       flash[:success] = message
       redirect_to user_profile_path(id: @user.id)
     end
-
 end
