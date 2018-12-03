2b682e9fa114e6acca7bd5e2a782263d78b7ff53
RyumaRyama <homes.ryu@gmail.com>
2018-12-03 21:17:21 +0900

    can setting element private

--- web/app/controllers/element_controller.rb
+++ web/app/controllers/element_controller.rb
@@ -25,21 +25,17 @@ class ElementController < ApplicationController
   def update
     @user = User.find_by(account: params[:account])
 
+    # 趣味の公開設定の変更
+    update_user_elements_params[:users_elements_attributes].each do |user_element_is_private|
+      user_element = UsersElement.find_by(id: user_element_is_private[1][:id])
+      @user.users_elements.find_by(element_id: user_element.element_id
+      ).update!(private: user_element_is_private[1][:private])
+    end
+
     # 趣味を削除
     update_user_elements_params[:elements_attributes].each do |element|
       if element[1][:_destroy] == "1"
         @user.elements.delete(Element.find_by(id: element[1][:id]))
-      end
-    end
-
-    # 趣味の公開設定の変更
-    update_user_elements_params[:users_elements_attributes].each do |user_element_is_private|
-      if not UsersElement.find_by(id: user_element_is_private[1][:id]).nil?
-        user_element = UsersElement.find_by(id: user_element_is_private[1][:id])
-        puts "失敗した"*100
-        p user_element
-        @user.users_elements.find_by(element_id: user_element.element_id
-        ).update!(private: user_element_is_private[1][:private])
       end
     end
 
@@ -53,9 +49,6 @@ class ElementController < ApplicationController
         elements: [:name],
         users_elements_attributes: [:private]
       )
-      # params.require(:elements).map do |param|
-      #   param.permit(:name)
-      # end
     end
 
     def update_user_elements_params
@@ -66,21 +59,43 @@ class ElementController < ApplicationController
     end
 
     def add_elements(message)
-      puts "とぅっとぅる〜"*100
-      # p element_params[:elements]
-      p element_params
-      puts "とぅっとぅる〜"*100
-      
-      element_params[:elements].each do |ep|
+      private_list = [] # privateにするelement
+      if element_params[:users_elements_attributes] != nil
+        element_params[:users_elements_attributes].each do |private|
+          private_list << private
+        end
+      end
+
+      element_params[:elements].size.times do |i|
+        add_element = true
+
+        # elementの追加
+        ep = element_params[:elements][i]
         @element = Element.new(ep)
         if @element.save
           @user.elements << Element.find_by(ep)
+        elsif Element.exists?(name: @element.name)     # Elementが既に存在する場合
+          @user.elements << Element.find_by(ep) unless @user.elements.exists?(name: @element.name)
         else
-          if Element.exists?(name: @element.name)     # Elementが既に存在する場合
-            @user.elements << Element.find_by(ep) unless @user.elements.exists?(name: @element.name)
+          add_element = false
+        end
+
+        # private設定
+        if add_element
+          users_element = @user.users_elements.find_by(
+            element_id: Element.find_by(name: @element.name).id)
+
+          if (not private_list.empty?) and private_list[0][0] == i.to_s     # privateとして設定するか
+            private_list.shift
+            # user_element = UsersElement.find_by(id: user_element_is_private[1][:id])
+            users_element.update!(private: true)
+          else
+            users_element.update!(private: false)
           end
         end
       end
+
+
       flash[:success] = message
       redirect_to user_profile_path(account: @user.account)
     end
