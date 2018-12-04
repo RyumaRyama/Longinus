fa8599cf4b3357bb06e6e7ee1c4501b5f2ea70ef
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-12-04 20:09:30 +0900

    errorページへの繊維

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   resources :element, param: :account
   resources :common_elements, param: :account
   resources :relationships, only: [:create, :destroy], param: :account
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
