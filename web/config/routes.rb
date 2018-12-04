a739b46a827a150d97cf49e0a148722226f8bca2
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-12-04 20:17:35 +0900

    routeから無駄な項目削除

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -17,7 +17,5 @@ Rails.application.routes.draw do
   resources :element, param: :account
   resources :common_elements, param: :account
   resources :relationships, only: [:create, :destroy], param: :account
-  get '*not_found' => 'application#routing_error'
-  post '*not_found' => 'application#routing_error'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
