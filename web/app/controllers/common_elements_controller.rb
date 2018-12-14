9b2574529eba2c6144334c3a277f8039aeca3390
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-12-14 17:05:41 +0900

    common_element関連ファイルの削除

--- web/app/controllers/common_elements_controller.rb
+++
@@ -1,7 +0,0 @@
-class CommonElementsController < ApplicationController
-  def show
-    friend = User.find_by(account: params[:account])
-    @friend_name = friend.name
-    @friend_elements = friend.elements.where(id: current_user.elements)
-  end
-end
--- web/test/controllers/common_elements_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CommonElementsControllerTest < ActionDispatch::IntegrationTest
-end
