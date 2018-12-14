5000d1ecab6a42fb5bf9ae2bd31861e3b986cb74
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-12-14 17:27:58 +0900

    コンフリクト解消

--- web/app/assets/javascripts/sharing.js
+++ web/app/assets/javascripts/sharing.js
@@ -1,16 +1,11 @@
 $(document).on('turbolinks:load',
 function create_link(){
   var link=document.URL;
-  document.getElementById('txt_share').value=link;
-  var clipboard = new Clipboard('#cp');
-  clipboard.on('success', function(e) {
-    e.clearSelection();
-    });
-    $('#code').empty();
-    $('#code').qrcode({
-      width: 100,
-      height: 100,
-      text: link
-    });
-    $('canvas').attr('id', 'code');
+  $('#code').empty();
+  $('#code').qrcode({
+    width: 100,
+    height: 100,
+    text: link
+  });
+  $('canvas').attr('id', 'code');
 });
